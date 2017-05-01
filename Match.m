57c6858a7abe9b3feab8a1e4f88b63b2497ced7f
Rob <robert.dale.morris@gmail.com>
2017-05-01 19:22:12 -0400

    subplots added

--- Match.m
+++ Match.m
@@ -1,3 +1,4 @@
+clc; clear; close all
 %%%
 % set names of query image and target image
 sdog_folder = './SampleDogs/';
@@ -18,46 +19,45 @@ d5=zeros(45,1);
 max=zeros(1,5);
 
 
-for i=1:1:5,
-    figure(i);
-    subplot(2,2,1)
-    imshow([sdog_folder,'d',num2str(i),'.png']);
-    maxCorrScore=0;
+for i=1:1:5
+    
     SampleDog=[sdog_folder, 'd',num2str(i),'.png'];
     Isd= imread(SampleDog);
     IcsdGrayScale = rgb2gray(Isd);
+    currCorrScore = -1000;
+    maxCorrScore = -1000;
     
-    for j=1:1:45,
-        
-    DBDogs=[dbdogs_folder,'dog',num2str(j),'.png'];
-    Idb= imread(DBDogs);
-    IqdbGrayScale = rgb2gray(Idb);
+    for j=1:1:45
 
-    %Compute the correlation match score
+        DBDogs=[dbdogs_folder,'dog',num2str(j),'.png'];
+        Idb= imread(DBDogs);
+        IqdbGrayScale = rgb2gray(Idb);
 
-    currCorrScore = myCorrelationMatch(IcsdGrayScale, IqdbGrayScale);
-    
-    %Store the correlation coefficient
-    
-    if i==1
-        d1(j,1)=currCorrScore;
-    elseif i==2
-        d2(j,1)=currCorrScore;
-    elseif i==3
-        d3(j,1)=currCorrScore;
-    elseif i==4
-        d4(j,1)=currCorrScore;
-    elseif i==5
-        d5(j,1)=currCorrScore;
+        %Compute the correlation match score
+
+        currCorrScore = myCorrelationMatch(IcsdGrayScale, IqdbGrayScale);
+
+        %Store the correlation coefficient
+
+        if i==1
+            d1(j,1)=currCorrScore;
+        elseif i==2
+            d2(j,1)=currCorrScore;
+        elseif i==3
+            d3(j,1)=currCorrScore;
+        elseif i==4
+            d4(j,1)=currCorrScore;
+        elseif i==5
+            d5(j,1)=currCorrScore;
+        end
+
+        %Pick the most three closely pics
+        if (currCorrScore > maxCorrScore) 
+              maxCorrScore= currCorrScore;
+        end
     end
-    
-    %Pick the most three closely pics
-    if (currCorrScore > maxCorrScore) 
-          maxCorrScore= currCorrScore;
-    end
-    end
-    max(1,i)=maxCorrScore;
-   
+        max(1,i)=maxCorrScore;
+
 end
 
 T=table(d1,d2,d3,d4,d5,...
@@ -67,54 +67,69 @@ T=table(d1,d2,d3,d4,d5,...
 mostThree=zeros(1,3);
 %Sorted the correlation 
 sorted_d1=sort(d1,'descend');
-sorted_d2=sort(d2,'descend');
+sorted_d2=sort(d2, 'descend');
 sorted_d3=sort(d3,'descend');
 sorted_d4=sort(d4,'descend');
 sorted_d5=sort(d5,'descend');
 %Get the name of the image
-for b = 1:1:k;
-    if i==1
+for b = 1:1:5
+    
+    figure(b);
+    subplot(2,2,1)
+    imshow([sdog_folder,'d',num2str(b),'.png']);
+    title(['Sample: ',num2str(b)]);
+    
+    if b==1
+    figure(b);
      for k=1:1:3
         for q=1:1:45
             if d1(q,1)==sorted_d1(k,1)
                 subplot(2,2,k+1);
                 imshow([origindogs_folder,'dog',num2str(q),'.png']);
+                title(['Dog: ',num2str(q),' Corr Score:', num2str(d1(q,1))]);
             end 
         end
      end
-    elseif i==2
-     for k=1:1:3
+     
+    elseif b==2
+     for p=1:1:3
         for q=1:1:45
-            if d2(q,1)==sorted_d2(k,1)
-                subplot(2,2,k+1);
+            if d2(q,1)==sorted_d2(p,1)
+                subplot(2,2,p+1);
                 imshow([origindogs_folder,'dog',num2str(q),'.png']);
+                title(['Dog: ',num2str(q),' Corr Score:', num2str(d2(q,1))])
             end 
         end
      end
-    elseif i==3
-     for k=1:1:3
+     
+    elseif b==3
+     for l=1:1:3
         for q=1:1:45
-            if d3(q,1)==sorted_d3(k,1)
-                subplot(2,2,k+1);
+            if d3(q,1)==sorted_d3(l,1)
+                subplot(2,2,l+1);
                 imshow([origindogs_folder,'dog',num2str(q),'.png']);
+                title(['Dog: ',num2str(q),' Corr Score:', num2str(d3(q,1))])
             end 
         end
      end
-    elseif i==4
-     for k=1:1:3
+    elseif b==4
+     for m=1:1:3
         for q=1:1:45
-            if d4(q,1)==sorted_d4(k,1)
-                subplot(2,2,k+1);
+            if d4(q,1)==sorted_d4(m,1)
+                subplot(2,2,m+1);
                 imshow([origindogs_folder,'dog',num2str(q),'.png']);
+                title(['Dog: ',num2str(q),' Corr Score:', num2str(d4(q,1))])                
             end 
         end
      end
-    elseif i==5
-     for k=1:1:3
+     
+    elseif b==5
+     for n=1:1:3
         for q=1:1:45
-            if d5(q,1)==sorted_d5(k,1)
-                subplot(2,2,k+1);
+            if d5(q,1)==sorted_d5(n,1)
+                subplot(2,2,n+1);
                 imshow([origindogs_folder,'dog',num2str(q),'.png']);
+                title(['Dog: ',num2str(q),' Corr Score:', num2str(d5(q,1))])
             end 
         end
      end
